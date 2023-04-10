18d6ec5bf8a390b1691638e65553f81f6aa9c884
Hervé TEGUIA <tkh.tegus@gmail.com>
2023-04-10 18:48:57 +0400

    Add project files.

---
+++ XamarinRefitTimeout/XamarinRefitTimeout/ICallApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Refit;
+
+namespace XamarinRefitTimeout
+{
+    [Headers("Content-Type: application/json", "Authorization: Bearer")]
+    public interface ICallApi
+    {
+        [Get("/weatherforecast")]
+        Task<HttpResponseMessage> GetAll(CancellationToken cancellationToken);
+    }
+}
---
+++ XamarinRefitTimeout/XamarinRefitTimeout/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Refit;
+using Xamarin.Forms;
+
+namespace XamarinRefitTimeout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string baseUrl = "XXXX";
+                var apiClient = RestService.For<ICallApi>(baseUrl);
+                var cts = new CancellationTokenSource();
+                cts.CancelAfter(TimeSpan.FromSeconds(10));
+                var response = await apiClient.GetAll(cts.Token);
+                var content = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<object>(content);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oups " + ex.Message);
+            }
+        }
+    }
+}
