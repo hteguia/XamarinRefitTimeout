323f5909f4c45c61ed2ac124d4f8efe2638c3ba8
Hervé TEGUIA <tkh.tegus@gmail.com>
2023-04-11 00:26:27 +0400

    up

--- XamarinRefitTimeout/XamarinRefitTimeout/MainPage.xaml.cs
+++ XamarinRefitTimeout/XamarinRefitTimeout/MainPage.xaml.cs
@@ -7,13 +7,17 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Plugin.Connectivity;
+using Polly;
 using Refit;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace XamarinRefitTimeout
 {
     public partial class MainPage : ContentPage
     {
+        public int conn = 5;
         public MainPage()
         {
             InitializeComponent();
@@ -23,19 +27,32 @@ namespace XamarinRefitTimeout
         {
             try
             {
-                string baseUrl = "XXXX";
-                var apiClient = RestService.For<ICallApi>(baseUrl);
-                var cts = new CancellationTokenSource();
-                cts.CancelAfter(TimeSpan.FromSeconds(10));
-                var response = await apiClient.GetAll(cts.Token);
-                var content = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<object>(content);
-            }
+                var policy = Policy.Handle<NoNetworkException>()
+                    .WaitAndRetryAsync(5, sleepDurationProvider => 
+                                             TimeSpan.FromSeconds(5));
 
+                await policy.ExecuteAsync(GetAllUser);
+            }
             catch (Exception ex)
             {
                 Console.WriteLine("Oups " + ex.Message);
             }
+        }
+
+        private async Task GetAllUser()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                throw new NoNetworkException("Connection to internet isn't available");
+            }
+
+            string baseUrl = "http://192.168.100.5:5272";
+            var apiClient = RestService.For<ICallApi>(baseUrl);
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(10));
+            var response = await apiClient.GetAll(cts.Token);
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<object>(content);
         }
     }
 }
---
+++ XamarinRefitTimeout/XamarinRefitTimeout/NoNetworkException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinRefitTimeout
+{
+    public class NoNetworkException : Exception
+    {
+        public NoNetworkException(string message) : base(message)
+        {
+        }
+    }
+}
